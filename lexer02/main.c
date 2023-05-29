2cc840276d4c96eeac85d4c9ed24073b242227f8
Stella Rufino <stellarufino@gmail.com>
2023-05-29 12:37:17 -0300

    final

--- lexer02/main.c
+++ lexer02/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//
+
 enum Token {
     Mais,
     Menos,
@@ -16,124 +16,88 @@ struct TokenList {
     struct TokenList *next;
 };
 
-void usar_token(char *sinalizador, struct TokenList **tokens) {
+void usar_token(const char *sinalizador, struct TokenList **tokens) {
     *tokens = NULL;
     struct TokenList *add_final = NULL;
-    while (*sinalizador != '\0') {
-        if (*sinalizador == '+') {
-            struct TokenList *node = malloc(sizeof(struct TokenList));
-            node->token = Mais;
-            node->next = NULL;
-            if (add_final == NULL) {
-                *tokens = node;
-            } else {
-                add_final->next = node;
-            }
-            add_final = node;
-        } else if (*sinalizador == '-') {
-            struct TokenList *node = malloc(sizeof(struct TokenList));
-            node->token = Menos;
-            node->next = NULL;
-            if (add_final == NULL) {
-                *tokens = node;
-            } else {
-                add_final->next = node;
-            }
-            add_final = node;
-            
-        }  else if (*sinalizador == '*') {
-            
-         if (*(sinalizador + 1 ) == '*') {
-            struct TokenList *node = malloc(sizeof(struct TokenList));
-            node->token = Potencia;
-            node->next = NULL;
-                if (add_final == NULL) {
-                    *tokens = node;
-                } else {
-                    add_final->next = node;
-                }
-                add_final = node;   
-             sinalizador++;
-             
-        } 
-         else {
-           struct TokenList *node = malloc(sizeof(struct TokenList));
-            node->token = Multiplicacao;
-            node->next = NULL;
-                if (add_final == NULL) {
-                    *tokens = node;
-                } else {
-                    add_final->next = node;
-                }
-                 add_final = node;
 
-         }
-           
-        }
-        
-         else if (*sinalizador == '/') {
-            struct TokenList *node = malloc(sizeof(struct TokenList));
+    while (*sinalizador != '\0') {
+        struct TokenList *node = malloc(sizeof(struct TokenList));
+        node->next = NULL;
+
+        if (*sinalizador == '+') {
+            node->token = Mais;
+        } else if (*sinalizador == '-') {
+            node->token = Menos;
+        } else if (*sinalizador == '*') {
+            if (*(sinalizador + 1) == '*') {
+                node->token = Potencia;
+                sinalizador++;
+            } else {
+                node->token = Multiplicacao;
+            }
+        } else if (*sinalizador == '/') {
             node->token = Divisao;
-            node->next = NULL;
-            if (add_final == NULL) {
-                *tokens = node;
-            } else {
-                add_final->next = node;
-            }
-            add_final = node;    
         } else if (*sinalizador != ' ') {
-            struct TokenList *node = malloc(sizeof(struct TokenList));
             node->token = Indeterminado;
-            node->next = NULL;
-            if (add_final == NULL) {
-                *tokens = node;
-            } else {
-                add_final->next = node;
-            }
-            add_final = node;
+        } else {
+            free(node);
+            sinalizador++;
+            continue;
         }
+
+        if (add_final == NULL) {
+            *tokens = node;
+        } else {
+            add_final->next = node;
+        }
+
+        add_final = node;
         sinalizador++;
     }
 }
 
 void imprimir_tokens(struct TokenList *tokens) {
     printf("Tokens: ");
+
     while (tokens != NULL) {
         switch (tokens->token) {
-        case Mais:
-            printf("Mais, ");
-            break;
-        case Menos:
-            printf("Menos, ");
-            break;
-        case Multiplicacao:
-            printf("Multiplicar, ");
-            break;
-        case Potencia:
-            printf("Potencia, ");
-            break;
-        case Divisao:
-            printf("Dividir, ");
-            break;
-        case Indeterminado:
-            printf("Indeterminado, ");
-            break;
+            case Mais:
+                printf("Mais, ");
+                break;
+            case Menos:
+                printf("Menos, ");
+                break;
+            case Multiplicacao:
+                printf("Multiplicar, ");
+                break;
+            case Potencia:
+                printf("Potencia, ");
+                break;
+            case Divisao:
+                printf("Dividir, ");
+                break;
+            case Indeterminado:
+                printf("Indeterminado, ");
+                break;
         }
+
         tokens = tokens->next;
     }
+
     printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-     FILE *fp = fopen("input.txt", "r");
+int main() {
+    FILE *fp = fopen("input.txt", "r");
     if (fp == NULL) {
-        printf("Erro ao abrir arquivo: %s\n", argv[1]);
+        printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
     char buffer[1024];
     struct TokenList *tokens = NULL;
-    while (fgets(buffer, 1024, fp) != NULL) {
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
         buffer[strcspn(buffer, "\n")] = '\0';  // remove o caractere de nova linha
         usar_token(buffer, &tokens);
         imprimir_tokens(tokens);
